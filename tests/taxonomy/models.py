5238e6af9eda675747e5819b0db70eff1c24341f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-02-23 17:18:03 +0600

    Revert "lets see"
    This reverts commit 7633faceb21d1d926804b445597fbf6af1e77715.

--- tests/taxonomy/models.py
+++ tests/taxonomy/models.py
@@ -4,7 +4,9 @@ from django_ltree.models import TreeModel
 
 
 class Taxonomy(TreeModel):
+    label_size = 2
+
     name = models.TextField()
 
     def __str__(self):
-        return f"{self.path}: {self.name}"
+        return '{}: {}'.format(self.path, self.name)
