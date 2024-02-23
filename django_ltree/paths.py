71b18b9cc3145ddab881d07eb8f21ae216afa701
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-02-23 19:57:59 +0600

    Update paths.py

--- django_ltree/paths.py
+++ django_ltree/paths.py
@@ -33,7 +33,7 @@ class PathGenerator(object):
     @staticmethod
     def guess_the_label_size(path_size: int, combination_size: int) -> int:
         # The theoritical limit for this at the time of writing is 2_538_557_185_841_324_496 (python 3.12.2)
-        calculated_path_size = 0
+        calculated_path_size = -1
         # The theoritical limit for this at the time of writing is 32 (python 3.12.2)
         label_size = 0
 
@@ -48,7 +48,7 @@ class PathGenerator(object):
             last = possible_cominations
             calculated_path_size += possible_cominations
 
-            if calculated_path_size > (path_size + 1) and label_size != 0:
+            if calculated_path_size > path_size and label_size != 0:
                 break
 
             label_size += 1
