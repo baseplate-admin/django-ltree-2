c706e4ff8959069f6067ce3f5a1d853ff6809f9d
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-02-23 19:26:29 +0600

    Update guess_label_size.py
    add
    
    Update guess_label_size.py
    
    Update guess_label_size.py
    
    Update guess_label_size.py
    
    add

--- django_ltree/paths.py
+++ django_ltree/paths.py
@@ -32,27 +32,26 @@ class PathGenerator(object):
 
     next = __next__
 
-    @staticmethod
-    def guess_the_label_size(path_size: int, combination_size: int):
-        if path_size == 0:
-            return 1
-
+    def guess_the_label_size(self, path_size: int, combination_size: int) -> int:
         # The theoritical limit for this at the time of writing is 2_538_557_185_841_324_496 (python 3.12.2)
-        calculated_path_size = +0
+        calculated_path_size = 0
         # The theoritical limit for this at the time of writing is 32 (python 3.12.2)
         label_size = 0
 
         # THIS IS AN VERY IMPORTANT CHECK
         last = 0
 
-        while calculated_path_size < path_size:
+        while True:
             possible_cominations = math.comb(combination_size, label_size)
-
             if last > possible_cominations:
                 raise ValueError("We approached the limit of `math.comb`")
 
             last = possible_cominations
             calculated_path_size += possible_cominations
+
+            if calculated_path_size > path_size and label_size != 0:
+                break
+
             label_size += 1
 
         return label_size
